c2af08be63cd45c371dc81d47530fd2d448fde93
kronseld <velikiygeolog@yandex.ru>
2024-04-30 15:38:11 +0400

    second

--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,35 @@
 
 const gamesRouter = require("express").Router();
-const { sendAllGames, deleteGame, addGameController } = require('../controllers/games');
-const { getAllGames } = require('../middlewares/games')
+const { sendAllGames, addGameController, sendUpdatedGames } = require('../controllers/games');
+const { getAllGames,
+        checkIsTitleInArray,
+        updateGamesArray,
+        updateGamesFile,
+        findGameById,
+        deleteGame 
+
+} = require('../middlewares/games')
 
 
-gamesRouter.post("/games", getAllGames, addGameController);
-gamesRouter.get("/games", getAllGames, sendAllGames)
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+gamesRouter.get("/games", getAllGames, sendAllGames);
 
-module.exports = gamesRouter;
+gamesRouter.post(
+  "/games",
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  sendUpdatedGames
+);
+
+gamesRouter.delete(
+  "/games/:id",
+  getAllGames,
+  findGameById,
+  deleteGame,
+  updateGamesFile,
+  sendUpdatedGames
+);
+
+module.exports = gamesRouter; 
  
